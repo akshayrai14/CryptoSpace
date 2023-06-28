ece627a41d2b5ff7300cebc89091d6e3c42f2972
Akshay Rai <109916723+akshayrai14@users.noreply.github.com>
2023-06-28 17:21:49 +0530

    Update LineChart.js
    console.log(Chart);

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -9,7 +9,7 @@ const { Title } = Typography;
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
-
+  console.log(Chart);
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
   }
